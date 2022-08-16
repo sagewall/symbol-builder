c6c4af3d59e00b96742bb684cf2e60da037b3707
Sage Wall <swall@esri.com>
2022-08-16 10:19:04 -0600

    🔥 Remove SimpleMarkerSymbol and SimpleFillSymbol

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,45 @@
 import "./style.css";
 
 import Color from "@arcgis/core/Color";
-import Point from "@arcgis/core/geometry/Point";
-import Polygon from "@arcgis/core/geometry/Polygon";
 import Polyline from "@arcgis/core/geometry/Polyline";
 import Graphic from "@arcgis/core/Graphic";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import Map from "@arcgis/core/Map";
-import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
-import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
 import MapView from "@arcgis/core/views/MapView";
-import { setAssetPath } from "@esri/calcite-components/dist/components";
-import "@esri/calcite-components/dist/components/calcite-color-picker";
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
 import Expand from "@arcgis/core/widgets/Expand";
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+import "@esri/calcite-components/dist/components/calcite-accordion";
+import "@esri/calcite-components/dist/components/calcite-accordion-item";
+import "@esri/calcite-components/dist/components/calcite-color-picker";
+import "@esri/calcite-components/dist/components/calcite-panel";
 
 setAssetPath("https://js.arcgis.com/calcite-components/1.0.0-beta.91/assets");
+
+const simpleLineSymbolPanel = document.createElement(
+  "calcite-panel"
+) as HTMLCalcitePanelElement;
+
+const simpleLineSymbolAccordian = document.createElement(
+  "calcite-accordion"
+) as HTMLCalciteAccordionElement;
+
+const simpleLineSymbolAccordianItem = document.createElement(
+  "calcite-accordion-item"
+) as HTMLCalciteAccordionItemElement;
+simpleLineSymbolAccordianItem.heading = "Color";
+simpleLineSymbolAccordian.appendChild(simpleLineSymbolAccordianItem);
 
 const simpleLineSymbolColorPicker = document.createElement(
   "calcite-color-picker"
 ) as HTMLCalciteColorPickerElement;
+simpleLineSymbolColorPicker.value = "#3eb33d";
+simpleLineSymbolAccordianItem.appendChild(simpleLineSymbolColorPicker);
+
+simpleLineSymbolPanel.appendChild(simpleLineSymbolAccordian);
 
 const simpleLineSymbol = new SimpleLineSymbol();
-
-const simpleMarkerSymbol = new SimpleMarkerSymbol({
-  outline: simpleLineSymbol,
-});
-
-const simpleFillSymbol = new SimpleFillSymbol({
-  outline: simpleLineSymbol,
-});
 
 const map = new Map({
   basemap: "hybrid",
@@ -56,20 +65,10 @@ view.ui.add(basemapGalleryExpand, {
   position: "top-left",
 });
 
-view.ui.add(simpleLineSymbolColorPicker, { position: "top-right" });
+view.ui.add(simpleLineSymbolPanel, { position: "top-right" });
 
 const graphicsLayer = new GraphicsLayer();
 map.add(graphicsLayer);
-
-const point = new Point({
-  longitude: -49.97,
-  latitude: 41.73,
-});
-
-const pointGraphic = new Graphic({
-  geometry: point,
-  symbol: simpleMarkerSymbol,
-});
 
 const polyline = new Polyline({
   paths: [
@@ -86,51 +85,11 @@ const polylineGraphic = new Graphic({
   symbol: simpleLineSymbol,
 });
 
-const polygon = new Polygon({
-  rings: [
-    [
-      [-64.78, 32.3],
-      [-66.07, 18.45],
-      [-80.21, 25.78],
-      [-64.78, 32.3],
-    ],
-  ],
-});
-
-const polygonGraphic = new Graphic({
-  geometry: polygon,
-  symbol: simpleFillSymbol,
-});
-
 const update = () => {
-  // Why doesn't this work???
-  // pointGraphic.symbol = simpleMarkerSymbol;
-  // polylineGraphic.symbol = simpleLineSymbol;
-  // polygonGraphic.symbol = simpleFillSymbol;
-
-  // graphicsLayer.graphics.removeAll();
-
-  // graphicsLayer.graphics.addMany([
-  //   pointGraphic,
-  //   polylineGraphic,
-  //   polygonGraphic,
-  // ]);
-
-  const newPointGraphic = pointGraphic.clone();
   const newPolylineGraphic = polylineGraphic.clone();
-  const newPolygonGraphic = polygonGraphic.clone();
-
-  newPointGraphic.symbol = simpleMarkerSymbol;
   newPolylineGraphic.symbol = simpleLineSymbol;
-  newPolygonGraphic.symbol = simpleFillSymbol;
-
   graphicsLayer.graphics.removeAll();
-
-  graphicsLayer.graphics.addMany([
-    newPointGraphic,
-    newPolylineGraphic,
-    newPolygonGraphic,
-  ]);
+  graphicsLayer.graphics.add(newPolylineGraphic);
 };
 
 const handleSimpleLineSymbolColorPickerChange = () => {
