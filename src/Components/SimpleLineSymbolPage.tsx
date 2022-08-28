cab35c636545474f5a69b0fcb75e9f36a6b897c6
Sage Wall <sagewall@users.noreply.github.com>
2022-08-27 20:59:15 -0600

    🎨 Wrap pages in a calcite shell

--- src/Components/SimpleLineSymbolPage.tsx
+++ src/Components/SimpleLineSymbolPage.tsx
@@ -1,12 +1,14 @@
+import { CalciteShell } from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-shell";
 import MapView from "./MapView";
 import ShellPanel from "./ShellPanel";
 
 function SimpleLineSymbolPage() {
   return (
-    <>
+    <CalciteShell>
       <MapView />
       <ShellPanel></ShellPanel>
-    </>
+    </CalciteShell>
   );
 }
 
