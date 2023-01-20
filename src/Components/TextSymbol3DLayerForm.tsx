1255328443cec4eeb53db93cd91c7153d605848f
Sage Wall <swall@esri.com>
2023-01-20 09:19:12 -0700

    add initial TextSymbol3DLayerForm

---
+++ src/Components/TextSymbol3DLayerForm.tsx
@@ -0,0 +1,136 @@
+import {
+  CalciteBlock,
+  CalciteInputNumber,
+  CalciteInputText,
+  CalciteLabel,
+  CalciteOption,
+  CalciteSelect,
+} from "@esri/calcite-components-react";
+import "@esri/calcite-components/dist/components/calcite-block";
+import "@esri/calcite-components/dist/components/calcite-color-picker";
+import "@esri/calcite-components/dist/components/calcite-input-number";
+import "@esri/calcite-components/dist/components/calcite-input-text";
+import "@esri/calcite-components/dist/components/calcite-label";
+import "@esri/calcite-components/dist/components/calcite-option";
+import "@esri/calcite-components/dist/components/calcite-select";
+import "@esri/calcite-components/dist/components/calcite-slider";
+import "@esri/calcite-components/dist/components/calcite-switch";
+import React, { useState } from "react";
+import FontForm from "./FontForm";
+import { blockStyles, labelStyles } from "./lib/styles";
+import {
+  FontDecoration,
+  FontStyle,
+  FontWeight,
+  HorizontalAlignment,
+  VerticalAlignment,
+} from "./lib/types";
+
+interface Props {
+  handleFontDecorationChange: (value: FontDecoration) => void;
+  handleFontFamilyChange: (value: string) => void;
+  handleFontSizeChange: (value: string) => void;
+  handleFontStyleChange: (value: FontStyle) => void;
+  handleFontWeightChange: (value: FontWeight) => void;
+  handleHorizontalAlignmentChange: (value: HorizontalAlignment) => void;
+  handleLineHeightChange: (value: string) => void;
+  handleTextChange: (value: string) => void;
+  handleVerticalAlignmentChange: (value: VerticalAlignment) => void;
+}
+
+const TextSymbol3DLayerForm = ({
+  handleFontDecorationChange,
+  handleFontFamilyChange,
+  handleFontSizeChange,
+  handleFontStyleChange,
+  handleFontWeightChange,
+  handleHorizontalAlignmentChange,
+  handleLineHeightChange,
+  handleTextChange,
+  handleVerticalAlignmentChange,
+}: Props) => {
+  const horizontalAlignmentOptions = ["center", "right", "left"];
+  const verticalAlignmentOptions = ["baseline", "top", "middle", "bottom"];
+
+  const [horizontalAlignment, setHorizontalAlignment] = useState("center");
+  const [lineHeight, setLineHeight] = useState("1");
+  const [text, setText] = useState("Hello World!");
+  const [verticalAlignment, setVerticalAlignment] = useState("baseline");
+
+  return (
+    <React.Fragment>
+      <CalciteBlock style={blockStyles} collapsible heading={"font"}>
+        <FontForm
+          handleDecorationChange={handleFontDecorationChange}
+          handleFamilyChange={handleFontFamilyChange}
+          handleSizeChange={handleFontSizeChange}
+          handleStyleChange={handleFontStyleChange}
+          handleWeightChange={handleFontWeightChange}
+        />
+      </CalciteBlock>
+
+      <CalciteLabel layout="default" style={labelStyles}>
+        horizontalAlignment
+        <CalciteSelect
+          label={"horizontalAlignment selection"}
+          onCalciteSelectChange={(event) => {
+            setHorizontalAlignment(event.target.value);
+            handleHorizontalAlignmentChange(
+              event.target.value as HorizontalAlignment
+            );
+          }}
+          value={horizontalAlignment}
+        >
+          {horizontalAlignmentOptions.map((option, index) => (
+            <CalciteOption key={index}>{option}</CalciteOption>
+          ))}
+        </CalciteSelect>
+      </CalciteLabel>
+
+      <CalciteLabel layout="default" style={labelStyles}>
+        lineHeight
+        <CalciteInputNumber
+          label={"lineHeight input"}
+          min={0}
+          onCalciteInputNumberChange={(event) => {
+            setLineHeight(event.target.value);
+            handleLineHeightChange(event.target.value);
+          }}
+          value={lineHeight}
+        ></CalciteInputNumber>
+      </CalciteLabel>
+
+      <CalciteLabel layout="default" style={labelStyles}>
+        text
+        <CalciteInputText
+          label={"text input"}
+          onCalciteInputTextChange={(event) => {
+            setText(event.target.value);
+            handleTextChange(event.target.value);
+          }}
+          value={text}
+        ></CalciteInputText>
+      </CalciteLabel>
+
+      <CalciteLabel layout="default" style={labelStyles}>
+        verticalAlignment
+        <CalciteSelect
+          label={"verticalAlignment selection"}
+          onCalciteSelectChange={(event) => {
+            setVerticalAlignment(event.target.value);
+            handleVerticalAlignmentChange(
+              event.target.value as VerticalAlignment
+            );
+          }}
+          value={verticalAlignment}
+        >
+          {verticalAlignmentOptions.map((option, index) => (
+            <CalciteOption key={index}>{option}</CalciteOption>
+          ))}
+        </CalciteSelect>
+      </CalciteLabel>
+    </React.Fragment>
+  );
+};
+
+export default TextSymbol3DLayerForm;
