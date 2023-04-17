372ca334cd4d93b2230f9d00b0ed6fe922973843
Sage Wall <swall@esri.com>
2023-04-17 10:06:39 -0600

    fix(sceneview): initial extent
    After the view's container is changed it looses its extent, zoom
    and other properties. Reinitializing the view every time seems like the quick
    fix.

--- src/components/lib/sceneview.ts
+++ src/components/lib/sceneview.ts
@@ -9,59 +9,59 @@ import LocalBasemapsSource from "@arcgis/core/widgets/BasemapGallery/support/Loc
 import Expand from "@arcgis/core/widgets/Expand";
 import { point } from "./geometry";
 
-const blankBasemapVectorTileLayer = new VectorTileLayer({
-  portalItem: {
-    id: "da7c2aa6b22a439fae31294413b5bc62"
-  }
-});
-
-const blankBasemap = new Basemap({
-  baseLayers: [blankBasemapVectorTileLayer],
-  thumbnailUrl:
-    "https://jsapi.maps.arcgis.com/sharing/rest/content/items/da7c2aa6b22a439fae31294413b5bc62/info/thumbnail/thumbnail1660688993675.png",
-  title: "Blank"
-});
-
-const map = new ArcMap({
-  basemap: "gray-vector"
-});
-
-const view = new ArcSceneView({
-  center: point,
-  map,
-  scale: 1000
-});
-
-const localBasemapsSource = new LocalBasemapsSource({
-  basemaps: [
-    blankBasemap,
-    Basemap.fromId("satellite"),
-    Basemap.fromId("hybrid"),
-    Basemap.fromId("dark-gray-vector"),
-    Basemap.fromId("gray-vector"),
-    Basemap.fromId("streets-vector"),
-    Basemap.fromId("streets-night-vector"),
-    Basemap.fromId("streets-navigation-vector"),
-    Basemap.fromId("topo-vector"),
-    Basemap.fromId("streets-relief-vector")
-  ]
-});
-
-const basemapGallery = new BasemapGallery({
-  view,
-  source: localBasemapsSource
-});
-
-const basemapGalleryExpand = new Expand({
-  view,
-  content: basemapGallery
-});
-
-view.ui.add(basemapGalleryExpand, {
-  position: "top-left"
-});
-
 export const createSceneView = async (parentElement: HTMLDivElement, graphics?: Collection<Graphic>) => {
+  const blankBasemapVectorTileLayer = new VectorTileLayer({
+    portalItem: {
+      id: "da7c2aa6b22a439fae31294413b5bc62"
+    }
+  });
+
+  const blankBasemap = new Basemap({
+    baseLayers: [blankBasemapVectorTileLayer],
+    thumbnailUrl:
+      "https://jsapi.maps.arcgis.com/sharing/rest/content/items/da7c2aa6b22a439fae31294413b5bc62/info/thumbnail/thumbnail1660688993675.png",
+    title: "Blank"
+  });
+
+  const map = new ArcMap({
+    basemap: "gray-vector"
+  });
+
+  const view = new ArcSceneView({
+    center: point,
+    map,
+    scale: 1000
+  });
+
+  const localBasemapsSource = new LocalBasemapsSource({
+    basemaps: [
+      blankBasemap,
+      Basemap.fromId("satellite"),
+      Basemap.fromId("hybrid"),
+      Basemap.fromId("dark-gray-vector"),
+      Basemap.fromId("gray-vector"),
+      Basemap.fromId("streets-vector"),
+      Basemap.fromId("streets-night-vector"),
+      Basemap.fromId("streets-navigation-vector"),
+      Basemap.fromId("topo-vector"),
+      Basemap.fromId("streets-relief-vector")
+    ]
+  });
+
+  const basemapGallery = new BasemapGallery({
+    view,
+    source: localBasemapsSource
+  });
+
+  const basemapGalleryExpand = new Expand({
+    view,
+    content: basemapGallery
+  });
+
+  view.ui.add(basemapGalleryExpand, {
+    position: "top-left"
+  });
+
   view.container = parentElement;
   view.graphics = graphics as Collection<Graphic>;
   return view;
