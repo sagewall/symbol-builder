c91f391c350fbb457d6f36738ae6e7aab3c07865
Sage Wall <swall@esri.com>
2023-06-02 14:14:08 -0600

    fix(mapview, sceneview): prevent zooming to graphics
    Refs: #5

--- src/components/SceneView.tsx
+++ src/components/SceneView.tsx
@@ -39,12 +39,6 @@ const SceneView = ({ graphics }: SceneViewProps) => {
       const loadGraphics = async () => {
         if (graphics) {
           view.graphics = graphics;
-          await view.when();
-          view.goTo(graphics).catch((error) => {
-            if (error.name !== "AbortError") {
-              console.error(error);
-            }
-          });
         }
       };
       loadGraphics();
