5d2993a8b4b8c847b3f65bddcaaee59126d4c810
Sage Wall <swall@esri.com>
2024-06-14 15:37:21 -0600

    chore: change base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/symbol-builder/",
+  base: "/",
   build: {
     chunkSizeWarningLimit: 2200
   },
