adfa54cb5c31b85250e73ad8ea8362379a0eb490
Sage Wall <swall@esri.com>
2024-06-14 16:00:32 -0600

    chore: add env base name to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  build: {
-    chunkSizeWarningLimit: 2200
-  },
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_NAME,
+    build: {
+      chunkSizeWarningLimit: 2200
+    },
+    plugins: [react()]
+  };
 });
