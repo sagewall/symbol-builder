54c9ed1bf23fad822679c016bbbccb81fe2d6ea4
Sage Wall <swall@esri.com>
2024-06-18 10:55:19 -0600

    feat: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -8,6 +9,14 @@ export default defineConfig(({ mode }) => {
     build: {
       chunkSizeWarningLimit: 2200
     },
-    plugins: [react()]
+    plugins: [
+      react(),
+      visualizer({
+        template: "treemap",
+        open: true,
+        gzipSize: true,
+        filename: "analyze.html"
+      })
+    ]
   };
 });
