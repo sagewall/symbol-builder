635f97dfb9a4ca7d78b06c4734bdb48abea79bfa
Sage Wall <swall@esri.com>
2024-06-21 09:35:26 -0600

    chore(deps): update dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
