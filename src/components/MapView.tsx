c2348c51af9f2c03ac11921f9a51edf043cecf00
Sage Wall <swall@esri.com>
2024-06-24 11:39:27 -0600

    feat: use map components

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,7 +1,11 @@
 import type Graphic from "@arcgis/core/Graphic";
 import type Collection from "@arcgis/core/core/Collection";
+import Point from "@arcgis/core/geometry/Point";
 import type ArcMapView from "@arcgis/core/views/MapView";
-import React, { useEffect, useRef, useState } from "react";
+import type { ArcgisMapCustomEvent } from "@arcgis/map-components";
+import { ArcgisMap, ArcgisPlacement } from "@arcgis/map-components-react";
+import { CalciteAction } from "@esri/calcite-components-react";
+import React, { useEffect, useState } from "react";
 
 const viewStyles = {
   height: "100%",
@@ -15,23 +19,7 @@ interface MapViewProps {
 const MapView = ({ graphics }: MapViewProps) => {
   const isSSR = typeof window === "undefined";
 
-  const viewDivRef = useRef<HTMLDivElement>(null);
-
   const [view, setView] = useState<ArcMapView | null>(null);
-
-  useEffect(() => {
-    if (viewDivRef.current) {
-      const loadMapView = async () => {
-        const { createMapView } = await import("./lib/mapview");
-        setView(await createMapView(viewDivRef.current as HTMLDivElement, graphics));
-      };
-      loadMapView();
-
-      return () => {
-        view && view.destroy();
-      };
-    }
-  }, []);
 
   useEffect(() => {
     if (view) {
@@ -44,8 +32,39 @@ const MapView = ({ graphics }: MapViewProps) => {
     }
   }, [view, graphics]);
 
+  const handleArcgisViewReadyChange = (event: ArcgisMapCustomEvent<void>) => {
+    const { view } = event.target;
+    if (view) {
+      view.center = new Point({ longitude: -117.1957098, latitude: 34.0564505 });
+      view.zoom = 18;
+      setView(view);
+    }
+  };
+
   return (
-    <React.Fragment>{!isSSR && <div style={viewStyles} ref={viewDivRef}></div>}</React.Fragment>
+    <React.Fragment>
+      {!isSSR && (
+        <ArcgisMap
+          basemap="gray-vector"
+          onArcgisViewReadyChange={(event) => {
+            handleArcgisViewReadyChange(event);
+          }}
+          style={viewStyles}
+        >
+          <ArcgisPlacement position="top-right">
+            <CalciteAction
+              icon="zoom-to-object"
+              scale="s"
+              text="Zoom to Graphics"
+              textEnabled
+              onClick={() => {
+                view?.goTo(view.graphics);
+              }}
+            ></CalciteAction>
+          </ArcgisPlacement>
+        </ArcgisMap>
+      )}
+    </React.Fragment>
   );
 };
 
