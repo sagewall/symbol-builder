fd02b2977fddbde190a09df4a234d7972d35b3f7
Sage Wall <swall@esri.com>
2024-06-25 14:59:04 -0600

    chore: update clipboard security policy

--- src/components/CIMSymbolShell.tsx
+++ src/components/CIMSymbolShell.tsx
@@ -9,7 +9,7 @@ const CIMSymbolShell = () => {
       <CalciteShell style={shellStyles}>
         <Header title="CIMSymbol" backButton></Header>
         <iframe
-          allow="clipboard-read; clipboard-write self https://esri.github.io/cim-symbol-builder-js/ https://sagewall.github.io/symbol-builder/;"
+          allow="clipboard-read; clipboard-write self https://esri.github.io/cim-symbol-builder-js/ https://sagewall.github.io/symbol-builder/ https://local.arcgis.com:8000/ https://*.arcgis.com;"
           src="https://esri.github.io/cim-symbol-builder-js/"
           style={iframeStyles}
           title="CIM Symbol Builder"
